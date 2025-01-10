b3b509d1679d7d36914c0a84bfa08c982da515c9
Muhammad Choudhary <choudha1@sheridancollege.ca>
2025-01-10 10:11:43 -0500

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Muhammad Choudhary on 2025-01-10.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
