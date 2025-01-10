c0809da49abd0677234bc3f5b71fe9c7810effb7
Muhammad Choudhary <choudha1@sheridancollege.ca>
2025-01-10 10:29:17 -0500

    Create the MovieListView

---
+++ MoviesApp/MovieListView.swift
@@ -0,0 +1,22 @@
+//
+//  MovieListView.swift
+//  MoviesApp
+//
+//  Created by Muhammad Choudhary on 2025-01-10.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+@StateObject private var viewModel = MovieListViewModel()
+var body: some View {
+NavigationView {
+List(viewModel.movies) { movie in
+NavigationLink(destination: MovieDetailView(movie: movie)) {
+MovieRowView(movie: movie)
+}
+}
+.navigationTitle("Movies")
+}
+}
+}
